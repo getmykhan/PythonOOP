c1b462312d795c1a5cb50f1061856e20b9de876a
getmykhan <mkhan7@stevens.edu>
2017-08-17 10:25:12 -0400

    Minor tweaks

--- class.py
+++ class.py
@@ -9,3 +9,9 @@ class Emp:
 employee_one = Emp('Mike', 'Tyson', 44)
 
 print(employee_one.fname)
+
+employee_two = Emp('Lalo', 'Lola', 27)
+
+print(employee_two.fname + employee_two.lname)
+
+print('Both their ages put together is: ', employee_one.age + employee_two.age)
